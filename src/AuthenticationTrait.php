c413179d8caf1222afc5dd392100793d310a6c0f
Sebastian Blum <github@sblum.de>
2018-03-30 19:08:29 +0200

    added test for assertAccessDenied

--- src/AuthenticationTrait.php
+++ src/AuthenticationTrait.php
@@ -4,20 +4,34 @@ namespace SBlum\TestTraits;
 
 use Symfony\Bundle\FrameworkBundle\Client;
 use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 trait AuthenticationTrait
 {
     private $firewall = 'main';
+    private $logInPath = null;
 
     /**
      * @param Client $client
      */
-    private function assertAccessDenied(Client $client)
+    private function assertAccessDenied(Client $client): void
     {
-        $this->assertTrue($client->getResponse()->isRedirect(), \sprintf('Status-Code war %s anstatt 302', $client->getResponse()->getStatusCode()));
-        $this->assertContains('/login', $client->getResponse()->headers->get('Location'));
+        $this->assertSame(
+            Response::HTTP_FOUND,
+            $client->getResponse()->getStatusCode(),
+            \sprintf('AccessDenied redirects to the login page, so status code 302 was expected. Got: "%d"', $client->getResponse()->getStatusCode())
+        );
+
+        if (!empty($this->logInPath)) {
+            $logInPath = $this->logInPath;
+        }
+
+        $this->assertTrue(
+            $client->getResponse()->isRedirect($logInPath),
+            \sprintf('Expected login path is "%s", but got: "%s"', $logInPath, $client->getResponse()->headers->get('Location'))
+        );
     }
 
     /**
@@ -25,7 +39,7 @@ trait AuthenticationTrait
      * @param string $username
      * @param array  $roles
      */
-    private function logIn(Client &$client, string $username, array $roles)
+    private function logIn(Client &$client, string $username, array $roles): void
     {
         /** @var Session $session */
         $session = $client->getContainer()->get('session');
@@ -41,7 +55,7 @@ trait AuthenticationTrait
     /**
      * @param Client $client
      */
-    private function logInAsAdmin(Client &$client)
+    private function logInAsAdmin(Client &$client): void
     {
         $this->logIn($client, 'admin', ['ROLE_ADMIN']);
     }
@@ -49,7 +63,7 @@ trait AuthenticationTrait
     /**
      * @param Client $client
      */
-    private function logInAsSuperAdmin(Client &$client)
+    private function logInAsSuperAdmin(Client &$client): void
     {
         $this->logIn($client, 'superadmin', ['ROLE_SUPER_ADMIN']);
     }
@@ -57,7 +71,7 @@ trait AuthenticationTrait
     /**
      * @param Client $client
      */
-    private function logInAsUser(Client &$client)
+    private function logInAsUser(Client &$client): void
     {
         $this->logIn($client, 'user', ['ROLE_USER']);
     }
@@ -65,8 +79,13 @@ trait AuthenticationTrait
     /**
      * @param string $firewall
      */
-    private function setFirewall(string $firewall)
+    private function setFirewall(string $firewall): void
     {
         $this->firewall = $firewall;
+    }
+
+    private function setLogInPath(string $logInPath): void
+    {
+        $this->logInPath = $logInPath;
     }
 }
--- tests/AuthenticationTraitTest.php
+++ tests/AuthenticationTraitTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\SBlum\TestTraits;
 
+use SBlum\TestTraits\AuthenticationTrait;
 use Symfony\Bundle\FrameworkBundle\Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
@@ -11,22 +14,37 @@ use Symfony\Component\Security\Core\Role\Role;
 
 class AuthenticationTraitTest extends WebTestCase
 {
-    public function testAuthenticationTrait()
-    {
-        $implementation = new AuthenticationTraitImplementation();
+    use AuthenticationTrait;
 
-        $this->assertTrue(\method_exists($implementation, 'logIn'));
-        $this->assertTrue(\method_exists($implementation, 'logInAsUser'));
-        $this->assertTrue(\method_exists($implementation, 'logInAsAdmin'));
-        $this->assertTrue(\method_exists($implementation, 'logInAsSuperAdmin'));
+    public function testAuthenticationTrait(): void
+    {
+        $this->assertTrue(\method_exists($this, 'assertAccessDenied'));
+        $this->assertTrue(\method_exists($this, 'logIn'));
+        $this->assertTrue(\method_exists($this, 'logInAsUser'));
+        $this->assertTrue(\method_exists($this, 'logInAsAdmin'));
+        $this->assertTrue(\method_exists($this, 'logInAsSuperAdmin'));
     }
 
-    public function testLogIn()
+    public function testAssertAccessDeniedWithLogInPath(): void
+    {
+        $this->setLogInPath('/my-login');
+        $redirectResponse = new RedirectResponse('/my-login');
+
+        /** @var Client $client */
+        $client = $this->createMock(Client::class);
+        $client
+            ->expects($this->atLeastOnce())
+            ->method('getResponse')
+            ->willReturn($redirectResponse);
+
+        $this->assertAccessDenied($client);
+    }
+
+    public function testLogIn(): void
     {
         $client = static::createClient();
 
-        $implementation = new AuthenticationTraitImplementation();
-        $implementation->delegateLogIn(
+        $this->logIn(
             $client,
             'my-username',
             ['ROLE_ADMIN']
@@ -38,12 +56,11 @@ class AuthenticationTraitTest extends WebTestCase
         $this->assertEquals([new Role('ROLE_ADMIN')], $token->getRoles());
     }
 
-    public function testLoginAsAdmin()
+    public function testLoginAsAdmin(): void
     {
         $client = static::createClient();
 
-        $implementation = new AuthenticationTraitImplementation();
-        $implementation->delegateLogInAsAdmin($client);
+        $this->logInAsAdmin($client);
 
         $token = $this->generateToken($client);
         $this->assertInstanceOf(UsernamePasswordToken::class, $token);
@@ -51,12 +68,11 @@ class AuthenticationTraitTest extends WebTestCase
         $this->assertEquals([new Role('ROLE_ADMIN')], $token->getRoles());
     }
 
-    public function testLoginAsSuperAdmin()
+    public function testLoginAsSuperAdmin(): void
     {
         $client = static::createClient();
 
-        $implementation = new AuthenticationTraitImplementation();
-        $implementation->delegateLogInAsSuperAdmin($client);
+        $this->logInAsSuperAdmin($client);
 
         $token = $this->generateToken($client);
         $this->assertInstanceOf(UsernamePasswordToken::class, $token);
@@ -64,25 +80,28 @@ class AuthenticationTraitTest extends WebTestCase
         $this->assertEquals([new Role('ROLE_SUPER_ADMIN')], $token->getRoles());
     }
 
-    public function testLoginAsUser()
+    public function testLoginAsUser(): void
     {
         $client = static::createClient();
 
-        $implementation = new AuthenticationTraitImplementation();
-        $implementation->delegateLogInAsAdmin($client);
+        $this->logInAsUser($client);
 
         $token = $this->generateToken($client);
         $this->assertInstanceOf(UsernamePasswordToken::class, $token);
-        $this->assertSame('admin', $token->getUsername());
-        $this->assertEquals([new Role('ROLE_ADMIN')], $token->getRoles());
+        $this->assertSame('user', $token->getUsername());
+        $this->assertEquals([new Role('ROLE_USER')], $token->getRoles());
     }
 
-    public function testSetFirewall()
+    public function testSetFirewall(): void
     {
-        $implementation = new AuthenticationTraitImplementation();
-        $implementation->delegateSetFirewall('my-firewall');
+        $this->setFirewall('my-firewall');
+        $this->assertAttributeSame('my-firewall', 'firewall', $this);
+    }
 
-        $this->assertAttributeSame('my-firewall', 'firewall', $implementation);
+    public function testSetLogInPath(): void
+    {
+        $this->setLogInPath('/my/login-path');
+        $this->assertAttributeSame('/my/login-path', 'logInPath', $this);
     }
 
     private function generateToken(Client $client, string $firewall = 'main'): TokenInterface
